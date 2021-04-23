88b6f3a7e280ecb1bc5c69ab97e54b2d035cc36a
Taka <78757699+TakTakTakaAWI@users.noreply.github.com>
2021-04-23 14:23:00 -0700

    Initial Commit

---
+++ src/dodgeData.js
@@ -0,0 +1,35 @@
+import Client from 'ftp';
+import fs from 'fs';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+let c = new Client();
+c.connect({
+    host: process.env.host,
+    user: process.env.user,
+    password: process.env.password,
+    secure: false
+});
+c.list("/", false, (err, item) => {
+    if (err) {
+        console.error(err);
+        return;
+    }
+    item.map((x, i) => {
+        console.log(`Getting ${x.name} - Created ${x.date}`);
+        c.get(x.name, (err, stream) => {
+            if (err) {
+                console.error(`${x.name} could not be downloaded: ${err}`);
+            } else {
+                stream.once('close', () => {});
+                if (!fs.existsSync(x.name)) {
+                    stream.pipe(fs.createWriteStream(x.name));
+                } else {
+                    console.warn(`Skipping ${x.name} file exists.`)
+                }
+            }
+        })
+    })
+});
+c.end();
